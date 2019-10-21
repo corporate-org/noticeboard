1289dd930fae2bb18483a4f8623139a2e1a0c967
Chris Stephenson <chris.stephenson@humanitec.com>
2019-10-21 18:02:33 +0200

    Fixited syntax error in database initialization script.

--- server/bin/init_db.js
+++ server/bin/init_db.js
@@ -1,12 +1,18 @@
 const db = require('../db');
 
+console.log("[init_db.js] Initializing database");
+console.log(`[init_db.js] PGUSER="${process.env.PGUSER}"`);
+console.log(`[init_db.js] PGHOST="${process.env.PGHOST}"`);
+console.log(`[init_db.js] PGDATABASE="${process.env.PGDATABASE}"`);,
+console.log(`[init_db.js] DATABASE_PORT="${process.env.DATABASE_PORT}"`);
+
 async function createDb  () {
   await db.query(`SET timezone = 'utc'`);
   await db.query(`CREATE TABLE IF NOT EXISTS notices (
     id          SERIAL PRIMARY KEY,
     title       TEXT NOT NULL,
     content     TEXT NULL,
-    created_at  TIMESTAMP`);
+    created_at  TIMESTAMP)`);
 }
 
 try {
@@ -14,3 +20,4 @@ try {
 } catch(error) {
   console.error(error);
 }
+console.log("[init_db.js] Initializing complete.");
